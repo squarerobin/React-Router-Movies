52d45a230d74f88e65d05e6d3f4367199b0fa902
squarerobin <squarerobin.82@gmail.com>
2020-01-16 04:48:09 +0100

    save
    not getting data from API (error 404)

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom'
+import Movie from "./Movies/Movie"
+import MovieList from "./Movies/MovieList"
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList] = useState( [] );
 
-  const addToSavedList = movie => {
+/*   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
-  };
+  }; */
 
   return (
     <div>
       <SavedList list={savedList} />
       
-      <Route exact path='/' component={MovieList} />
+      <Route  path='/' component={MovieList} />
       <Route   path='/movies/:id' component={Movie} />
 
 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,50 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
+        .get("http://localhost:3000/api/movies")
         .then(response => {
           setMovies(response.data);
         })
         .catch(error => {
-          console.error('Server Error', error);
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} movie={movie} to={`/movies/${movie.id}`} />
       ))}
     </div>
   );
-}
+};
 
-function MovieDetails({ movie }) {
+function MovieDetails({ movie, props }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+      <Link className="movie-card" to={props.to}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </Link>
     </div>
   );
 }
