80be83ad2c53f17572b8e59dc4f404e11e780ced
squarerobin <squarerobin.82@gmail.com>
2020-01-16 02:24:56 +0100

    part 1 done

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,11 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      <Route exact path='/' component={MovieList} />
+      <Route   path='/movies/:id' component={Movie} />
+
+
     </div>
   );
 };
